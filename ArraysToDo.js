3a4dd3f7aea325efbd4791c69cbd1da27cdd46cd
Scott Nelson <scott.shannon.nelson@gmail.com>
2022-09-04 14:09:44 -0600

    ArraysToDo Commit

---
+++ ArraysToDo.js
@@ -0,0 +1,56 @@
+var arr1 = [5,3,8,10,12,13]
+var arr2 = [12,14,20,23,91,97]
+
+function pushFront(arr,val) {
+    arr = [val, ...arr];
+    return arr
+}
+
+function altPushFront(arr,value){
+    for(var i = arr.length; i<=0; i--){
+        arr[i] = arr[i-1]
+    }
+    arr[0] = value
+    return arr
+}
+
+console.log(pushFront(arr1,2))
+
+function reverseArr(arr) {
+    var r = arr.length - 1;
+    for (var i = 0; i <= r / 2; i++){
+        var temp = arr[i];
+        arr[i] = arr[r - i];
+        arr[r - i] = temp;
+    }
+    return arr
+}
+
+function insertAt(arr,index,value){
+    temp1 = arr[0]
+    temp2 = arr[arr.length - 1]
+    temp3 = arr[index -1]
+    for(var i = arr.length - 1; i> index; i--){
+        arr[i] = arr[i - 1]
+    }
+    arr[index] = value
+    arr.push(temp2)
+    arr[0] = temp1
+    return arr
+}
+
+function popFront(arr) {
+    reverseArr(arr);
+    console.log(arr[arr.length - 1])
+    arr.pop();
+    reverseArr(arr);
+    return arr
+}
+
+console.log(popFront(arr1))
+
+console.log(insertAt(arr1,2,115))
+
+console.log(insertAt(arr2,4,31))
+
+console.log(altPushFront(arr2,9))
